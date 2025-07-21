c6e006ffe55f8f5c4d01f0daab7731d78b6f73cf
Robin <robinyilmaz99@gmail.com>
2025-07-21 19:43:51 +0300

    Data Transformation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationPipeline
 
 STAGE_NAME = "Data Ingestion"
 
@@ -7,7 +8,20 @@ try:
     logger.info(F"stage {STAGE_NAME} Initiated")
     data_ingestion_pipeline = DataIngestionPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
-    logger.info(F"stage {STAGE_NAME} Completed")
+    logger.info(F">>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<<\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Transformation"
+try:
+    logger.info(F"stage {STAGE_NAME} Initiated")
+    data_transformation_pipeline = DataTransformationPipeline()
+    data_transformation_pipeline.initiate_data_transformation()
+    logger.info(F">>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<<\n")
 
 except Exception as e:
     logger.exception(e)

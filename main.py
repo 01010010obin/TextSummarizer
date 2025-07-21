7e22be7eb8b9907aed0d7b750282509492d44779
Robin <robinyilmaz99@gmail.com>
2025-07-21 23:18:22 +0300

    Model Training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationPipeline
+from src.textSummarizer.pipeline.model_trainer_pipeline import ModelTrainerPipeline
 
 STAGE_NAME = "Data Ingestion"
 
@@ -21,6 +22,18 @@ try:
     logger.info(F"stage {STAGE_NAME} Initiated")
     data_transformation_pipeline = DataTransformationPipeline()
     data_transformation_pipeline.initiate_data_transformation()
+    logger.info(F">>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<<\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer"
+try:
+    logger.info(F"stage {STAGE_NAME} Initiated")
+    model_trainer_pipeline = ModelTrainerPipeline()
+    model_trainer_pipeline.initiate_model_trainer()
     logger.info(F">>>>>>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<<<<\n")
 
 except Exception as e:
